2006c7ea409e1df2d3555622e0db2ecd38dcbae2
Alexandre Trotel <trotelalexandre@proton.me>
2025-12-10 23:06:44 +0100

    fix: review url for brew installation

--- Formula/todo-tree.rb
+++ Formula/todo-tree.rb
@@ -10,24 +10,24 @@ class TodoTree < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/alexandretrotel/todo-tree/releases/download/v#{version}/todo-tree-aarch64-apple-darwin.tar.gz"
+      url "https://github.com/alexandretrotel/todo-tree/releases/download/todo-tree-v#{version}/todo-tree-aarch64-apple-darwin.tar.gz"
       sha256 'PLACEHOLDER_ARM64_SHA256'
     end
 
     on_intel do
-      url "https://github.com/alexandretrotel/todo-tree/releases/download/v#{version}/todo-tree-x86_64-apple-darwin.tar.gz"
+      url "https://github.com/alexandretrotel/todo-tree/releases/download/todo-tree-v#{version}/todo-tree-x86_64-apple-darwin.tar.gz"
       sha256 'PLACEHOLDER_X86_64_SHA256'
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/alexandretrotel/todo-tree/releases/download/v#{version}/todo-tree-aarch64-unknown-linux-gnu.tar.gz"
+      url "https://github.com/alexandretrotel/todo-tree/releases/download/todo-tree-v#{version}/todo-tree-aarch64-unknown-linux-gnu.tar.gz"
       sha256 'PLACEHOLDER_LINUX_ARM64_SHA256'
     end
 
     on_intel do
-      url "https://github.com/alexandretrotel/todo-tree/releases/download/v#{version}/todo-tree-x86_64-unknown-linux-gnu.tar.gz"
+      url "https://github.com/alexandretrotel/todo-tree/releases/download/todo-tree-v#{version}/todo-tree-x86_64-unknown-linux-gnu.tar.gz"
       sha256 'PLACEHOLDER_LINUX_X86_64_SHA256'
     end
   end
@@ -41,4 +41,3 @@ class TodoTree < Formula
     system "#{bin}/todo-tree", '--version'
   end
 end
-
