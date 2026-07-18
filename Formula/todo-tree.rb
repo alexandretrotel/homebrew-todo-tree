96eb8d8086f27be147b95b0700f049225f123837
atrde <113524174+atrtde@users.noreply.github.com>
2026-07-18 14:56:44 +0200

    chore: update todo-tree to 0.6.0

--- Formula/todo-tree.rb
+++ Formula/todo-tree.rb
@@ -4,30 +4,30 @@
 class TodoTree < Formula
   desc "A CLI tool to find and display TODO-style comments in your codebase"
   homepage "https://github.com/atrtde/todo-tree"
-  version "0.5.3"
+  version "0.6.0"
   license "GPL-3.0-or-later"
 
   on_macos do
     on_arm do
-      url "https://github.com/atrtde/todo-tree/releases/download/todo-tree-v#{version}/todo-tree-aarch64-apple-darwin.tar.gz"
-      sha256 "9122a5fdd8694f035ca41cb434ee5560603ad1ec46f26ea790eb89dd8b81fe37"
+      url "https://github.com/atrtde/todo-tree/releases/download/v#{version}/todo-tree-aarch64-apple-darwin.tar.gz"
+      sha256 "1d6387a22f8d13e756f56bb7d668fd6282b591d2f5442994ecbd2f528358f2d8"
     end
 
     on_intel do
-      url "https://github.com/atrtde/todo-tree/releases/download/todo-tree-v#{version}/todo-tree-x86_64-apple-darwin.tar.gz"
-      sha256 "08121ec77a203b92d7bb5d2c7893a4d840642a2e08c0724110366a6951060c3e"
+      url "https://github.com/atrtde/todo-tree/releases/download/v#{version}/todo-tree-x86_64-apple-darwin.tar.gz"
+      sha256 "6333bd4fdfcb2f8b22e2722e9f8b075a4a41390331d8e1bd5a5464e24b53f963"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/atrtde/todo-tree/releases/download/todo-tree-v#{version}/todo-tree-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "b5d70fe01722dec079e5e7baebc81ce5e1d0dba0eea83c6a68573ec4a753d499"
+      url "https://github.com/atrtde/todo-tree/releases/download/v#{version}/todo-tree-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "f4bcc813cbde0b59ac754b4f7bff41dd9a3de02b41c3d82854c50061f8d3a911"
     end
 
     on_intel do
-      url "https://github.com/atrtde/todo-tree/releases/download/todo-tree-v#{version}/todo-tree-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "aab2334398e397382d8c055d50f582e263e86667c7d333e1748920c806188d6a"
+      url "https://github.com/atrtde/todo-tree/releases/download/v#{version}/todo-tree-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "52849ecee37757b4082836eb5be2b2c929e3838d8f50e8e42e9cf3af6b04f12a"
     end
   end
 
