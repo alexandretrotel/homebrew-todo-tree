1fc7d8b3f11b730ff9ab951118c8b77c98ece195
alexandretrotel <trotelalexandre@proton.me>
2026-08-11 10:44:41 +0200

    fix: rework formula and tests

--- Formula/todo-tree.rb
+++ Formula/todo-tree.rb
@@ -12,24 +12,24 @@ class TodoTree < Formula
   on_macos do
     on_arm do
       url "https://github.com/alexandretrotel/todo-tree/releases/download/v#{version}/todo-tree-aarch64-apple-darwin.tar.gz"
-      sha256 "fb055583e68e3354823f486205d93e1175110fd2951668621da9010bba85c374"
+      sha256 "4d4f34c5b1ba6b70682a1465d1983e07d25bb6a1dff78611e9211caa1c755e05"
     end
 
     on_intel do
       url "https://github.com/alexandretrotel/todo-tree/releases/download/v#{version}/todo-tree-x86_64-apple-darwin.tar.gz"
-      sha256 "28acde9f9e4adedf44cc6ab24e46b5b32050ab18a29ec681de6f4656802bbd47"
+      sha256 "5a0b452da0a01f379089b423bba5b03a378a58ff2761a31ccbade6b30587df9e"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/alexandretrotel/todo-tree/releases/download/v#{version}/todo-tree-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "9e60958468c876860f8c69fa39d9d02f51995ec8e00aa1ac2b313b3f5f2f189d"
+      sha256 "10a6c6a18e1cb55f1627b21e4b7dd67bf6c64e3b0a2430cf98f827094ad5562f"
     end
 
     on_intel do
       url "https://github.com/alexandretrotel/todo-tree/releases/download/v#{version}/todo-tree-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "052a9016d108d3cc351d59be024d89a2cc525bbf04cc9d540cb410119cb92432"
+      sha256 "25d379bfd4665081fb97b24d0b13d94c1e0397b2a95d85b0d3c7001670724338"
     end
   end
 
