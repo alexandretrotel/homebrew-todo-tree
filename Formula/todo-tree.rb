31c8d12e15826a291793e43ee23c8d4364cde5b5
alexandretrotel <trotelalexandre@proton.me>
2026-08-11 11:15:15 +0200

    fix: rework ruby

--- Formula/todo-tree.rb
+++ Formula/todo-tree.rb
@@ -1,7 +1,6 @@
 class TodoTree < Formula
-  desc "A command-line tool to find and display TODO-style comments in your codebase"
+  desc "Command-line tool to find and display TODO-style comments in your codebase"
   homepage "https://github.com/alexandretrotel/todo-tree"
-  version "1.0.0"
   license "GPL-3.0-or-later"
 
   livecheck do
@@ -11,24 +10,24 @@ class TodoTree < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/alexandretrotel/todo-tree/releases/download/v#{version}/todo-tree-aarch64-apple-darwin.tar.gz"
+      url "https://github.com/alexandretrotel/todo-tree/releases/download/v1.0.0/todo-tree-aarch64-apple-darwin.tar.gz"
       sha256 "4d4f34c5b1ba6b70682a1465d1983e07d25bb6a1dff78611e9211caa1c755e05"
     end
 
     on_intel do
-      url "https://github.com/alexandretrotel/todo-tree/releases/download/v#{version}/todo-tree-x86_64-apple-darwin.tar.gz"
+      url "https://github.com/alexandretrotel/todo-tree/releases/download/v1.0.0/todo-tree-x86_64-apple-darwin.tar.gz"
       sha256 "5a0b452da0a01f379089b423bba5b03a378a58ff2761a31ccbade6b30587df9e"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/alexandretrotel/todo-tree/releases/download/v#{version}/todo-tree-aarch64-unknown-linux-gnu.tar.gz"
+      url "https://github.com/alexandretrotel/todo-tree/releases/download/v1.0.0/todo-tree-aarch64-unknown-linux-gnu.tar.gz"
       sha256 "10a6c6a18e1cb55f1627b21e4b7dd67bf6c64e3b0a2430cf98f827094ad5562f"
     end
 
     on_intel do
-      url "https://github.com/alexandretrotel/todo-tree/releases/download/v#{version}/todo-tree-x86_64-unknown-linux-gnu.tar.gz"
+      url "https://github.com/alexandretrotel/todo-tree/releases/download/v1.0.0/todo-tree-x86_64-unknown-linux-gnu.tar.gz"
       sha256 "25d379bfd4665081fb97b24d0b13d94c1e0397b2a95d85b0d3c7001670724338"
     end
   end
